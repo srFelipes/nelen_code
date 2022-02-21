281168e58c99bf7c2bfff9a9348a045e2a41c0a0
Felipe Guajardo <fgerman.mena@gmail.com>
2022-02-20 22:52:29 -0300

    second commit

--- homing.py
+++ homing.py
@@ -1,36 +1,15 @@
 #!/home/rosconcoco/nelen/bin/python
+
+import time
+
 import odrive
 from odrive.enums import * 
 from odrive.utils import *
 
-odrvc = odrive.find_any(serial_number="207135A1524B") # codo and z
-odrvh = odrive.find_any(serial_number="206E3591524B") # hombro
-
-exec(open("./setup_config.py").read())
-# Settings
-#odrvc.axis0.controller.config.pos_gain = 13 #saved
-#odrvc.axis0.controller.config.vel_limit = 200 #saved
-#odrvc.axis0.trap_traj.config.vel_limit = 200 #saved
-#odrvc.axis1.controller.config.vel_limit = 4.35/1.5 #saved
-#odrvc.axis1.trap_traj.config.vel_limit = 4.35/1.5 #saved
-#odrvc.axis1.controller.config.inertia = 0.17/4 #saved
-#odrvh.axis0.controller.config.vel_limit = 2.5/1.5 #saved
-#odrvh.axis0.trap_traj.config.vel_limit = 2.5/1.5 #saved
-#odrvh.axis0.controller.config.inertia = 0.84/8 #saved 
-
-#odrvc.axis0.controller.config.input_mode = INPUT_MODE_TRAP_TRAJ #saved
-#odrvc.axis1.controller.config.input_mode = INPUT_MODE_TRAP_TRAJ #saved
-#odrvh.axis0.controller.config.input_mode = INPUT_MODE_TRAP_TRAJ #saved
-
-#odrvh.axis0.min_endstop.config.debounce_ms = 50 #saved
-
-# Offset update
-#odrvc.axis0.min_endstop.config.offset = .5 #saved
-#odrvc.axis1.min_endstop.config.offset = .2 #saved
-#odrvh.axis0.min_endstop.config.offset = .2 #saved
-
-# Then.. 
-import time
+from nelen_code.config import config
+from nelen_code.utils import exceptions
+odrvc = odrive.find_any(serial_number=config.rc_serial) # codo and z
+odrvh = odrive.find_any(serial_number=config.hombro_serial) # hombro
 
 print('Dumping errors codo-muneca')
 dump_errors(odrvc, True)
@@ -39,14 +18,41 @@ dump_errors(odrvh, True)
 
 # TODO: Generalize calibration and close_loop_control access
 
+
+
+def calibrate(axis):
+    #this should check if the axis changed correctly
+    axis.requested_state = AXIS_STATE_ENCODER_OFFSET_CALIBRATION  
+    exceptions.odrive_state(axis,AXIS_STATE_ENCODER_OFFSET_CALIBRATION)
+    exceptions.timeout_except(axis,AXIS_STATE_ENCODER_OFFSET_CALIBRATION)
+    print('succesfully entered AXIS_STATE_ENCODER_OFFSET_CALIBRATION')  
+    
+    #execute homing
+    axis.requested_state = AXIS_STATE_HOMING 
+    exceptions.raise_except(axis,AXIS_STATE_HOMING)
+    exceptions.timeout_except(axis,AXIS_STATE_HOMING)
+    print('Current axis successfully homed')
+    
+    #execute control loop
+    axis.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+    exceptions.raise_except(axis, AXIS_STATE_CLOSED_LOOP_CONTROL)
+    print('Current axis successfully enters control mod3')
+
+
+
+
 ## Codo-muneca
 # axis0
 print('Working on z')
 current_state_0 = odrvc.axis0.current_state
+
+#only the encoder must be calibrated, the motor parameters are saved in the odrive
+odrvc.axis0.requested_state = AXIS_STATE_ENCODER_OFFSET_CALIBRATION  
+
 while (current_state_0 == 1):
-    odrvc.axis0.requested_state = 7
+    odrvc.axis0.requested_state = AXIS_STATE_ENCODER_OFFSET_CALIBRATION
     time.sleep(12)
-    odrvc.axis0.requested_state = 11
+    odrvc.axis0.requested_state = AXIS_STATE_HOMING
     time.sleep(15)
     current_state_0 = odrvc.axis0.current_state
     if current_state_0 == 1:
--- setup_config.py
+++
@@ -1,4 +0,0 @@
-should_comeback= True
-offset_z_stg = -3.25 # 6.25
-offset_c_stg = -2.6
-offset_h_stg = -1.75
