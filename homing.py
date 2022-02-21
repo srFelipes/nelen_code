ed851dba2c75b37c11e01031b512e13d0bb4330b
Felipe Guajardo <fgerman.mena@gmail.com>
2022-02-20 23:41:16 -0300

    generalized calibration and homming routine

--- homing.py
+++ homing.py
@@ -21,6 +21,7 @@ dump_errors(odrvh, True)
 
 
 def calibrate(axis):
+    #only the encoder must be calibrated, the motor parameters are saved in the odrive
     #this should check if the axis changed correctly
     axis.requested_state = AXIS_STATE_ENCODER_OFFSET_CALIBRATION  
     exceptions.odrive_state(axis,AXIS_STATE_ENCODER_OFFSET_CALIBRATION)
@@ -39,79 +40,6 @@ def calibrate(axis):
     print('Current axis successfully enters control mod3')
 
 
-
-
-## Codo-muneca
-# axis0
-print('Working on z')
-current_state_0 = odrvc.axis0.current_state
-
-#only the encoder must be calibrated, the motor parameters are saved in the odrive
-odrvc.axis0.requested_state = AXIS_STATE_ENCODER_OFFSET_CALIBRATION  
-
-while (current_state_0 == 1):
-    odrvc.axis0.requested_state = AXIS_STATE_ENCODER_OFFSET_CALIBRATION
-    time.sleep(12)
-    odrvc.axis0.requested_state = AXIS_STATE_HOMING
-    time.sleep(15)
-    current_state_0 = odrvc.axis0.current_state
-    if current_state_0 == 1:
-        print('Current axis successfully homed')
-        odrvc.axis0.requested_state = 8
-        current_state_0 = odrvc.axis0.current_state
-        time.sleep(1)
-        if current_state_0 == 8: 
-            print('Current axis successfully enters control mod3')
-    else:
-        print(f'Control mode access failed. Current axis state = {current_state_0}')
-        dump_errors(odrvc, True)
-        time.sleep(1)
-
-# axis1
-print('Working on codo')
-current_state_1 = odrvc.axis1.current_state
-while (current_state_1 == 1):
-    odrvc.axis1.requested_state = 7
-    time.sleep(12)
-    odrvc.axis1.requested_state = 11
-    time.sleep(15)
-    current_state_1 = odrvc.axis1.current_state
-    if odrvc.axis1.current_state == 1:
-        print('Current axis successfully homed')
-        odrvc.axis1.requested_state = 8
-        time.sleep(1)
-        current_state_1 = odrvc.axis1.current_state
-        if current_state_1 == 8:  
-            print('Current axis successfully enters control mod3')
-    else:
-        print(f'Control mode access failed. Current axis state = {current_state_1}')
-        dump_errors(odrvc, True)
-        time.sleep(1)
-
-## Hombro
-print('Working on Hombro')
-# axis 0
-current_state_h = odrvh.axis0.current_state
-while (current_state_h == 1):
-    odrvh.axis0.requested_state = 7
-    time.sleep(12)
-    odrvh.axis0.requested_state = 11
-    time.sleep(15) 
-    current_state_h = odrvh.axis0.current_state
-    if current_state_h == 1:
-        print('Current axis successfully homed')
-        odrvh.axis0.requested_state = 8
-        time.sleep(1)
-        current_state_h = odrvh.axis0.current_state
-        if (current_state_h == 8):
-            print('Current axis successfully enters control mod3')
-            current_state_h = odrvh.axis0.current_state
-
-    else:
-        print(f'Homing failed. Current axis state = {current_state_h}')
-        dump_errors(odrvh, True)
-        time.sleep(1)
-
 # straight
 # z
 if should_comeback:
@@ -127,3 +55,22 @@ if should_comeback:
     odrvh.axis0.controller.move_incremental(offset_h_stg, False)
     time.sleep(5)
 print("fin")
+
+if __name__=='__init__':
+    ## Hombro
+    print('Working on Hombro')
+    calibrate(odrvh.axis0)
+    print('hombro calibrated')
+    
+    # odrv radio cubito, axis 0->Z, 1->codo
+    
+    #Z
+    print('Working on z')
+    calibrate(odrvc.axis0)
+    print('z calibrated')
+    
+    #codo
+    print('Working on codo')
+    calibrate(odrvc.axis1)
+    print('hombro calibrated')
+
